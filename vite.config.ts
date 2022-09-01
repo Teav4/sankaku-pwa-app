43b9a5e33e274b75dbcd93d3ab9375f5ef24ed8e
teav4 <teav4@feadorra.com>
2022-09-01 13:14:34 +0700

    build: setup prettier & manifest pwa

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,24 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      manifest: {
+        name: "Sankaku App",
+        short_name: "sankaku",
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'public/icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ]
 })
